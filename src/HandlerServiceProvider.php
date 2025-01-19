a332ea60fa64d3e4450b9bdef5d49071f37efecd
teksite <teksite.design@gmail.com>
2025-01-19 16:39:41 +0330

    change config file name

--- src/HandlerServiceProvider.php
+++ src/HandlerServiceProvider.php
@@ -8,18 +8,18 @@ class HandlerServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $configPath = config_path('cms-settings.php'); // Path to the published file
+        $configPath = config_path('handler-settings.php'); // Path to the published file
 
         $this->mergeConfigFrom(
-            file_exists($configPath) ? $configPath : __DIR__ . '/config/cms-settings.php', 'cms-settings');
+            file_exists($configPath) ? $configPath : __DIR__ . '/config/handler-settings.php', 'handler-settings');
     }
 
 
     public function boot(): void
     {
         $this->publishes([
-                __DIR__ . '/config/cms-settings.php' => config_path('cms-settings.php')
-            ],'config');
+                __DIR__ . '/config/handler-settings.php' => config_path('handler-settings.php')
+            ],'handler-settings');
     }
 
 
