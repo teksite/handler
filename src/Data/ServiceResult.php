abbe3c5315a2be4151ae942437c43ad091ce6e10
teksite <teksite.design@gmail.com>
2026-08-30 21:34:57 +0330

    add parameter ServiceResult

--- src/Data/ServiceResult.php
+++ src/Data/ServiceResult.php
@@ -9,8 +9,15 @@ final readonly class ServiceResult implements contract
     /**
      * @param bool $success
      * @param mixed $result
+     * @param string|array|null $errors
+     * @param int|null $successStatus
+     * @param int|null $failedStatus
      */
-    public function __construct(public bool $success, public mixed $result)
-    {
-    }
+    public function __construct(
+        public bool              $success,
+        public mixed             $result,
+        public string|array|null $errors = null,
+        public ?int              $successStatus = null,
+        public ?int              $failedStatus = null,
+    ) {}
 }
